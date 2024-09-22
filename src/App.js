93f5a568dcfbbec61fdd5af4a7dbe28b8ad7955f
ashleylyh <ylils@connect.ust.hk>
2024-09-22 20:16:28 +0800

    update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Routes,
   Navigate,
@@ -31,7 +31,7 @@ function App() {
   }, []);
 
   return (
-    <Router basename="/portfolio">
+    <Router>
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
